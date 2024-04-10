a49203a3d1362a91d5c66e7a00afc1eababb83a2
kakit <chankakit@live.com>
2024-04-10 14:08:40 +0800

    该用sanity的cdn api

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ const props = defineEmits({
   stockDialogShow: Boolean,
 })
 
-const sanityApi = 'https://ilbimbym.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27temp%27%5D%7Bname%2C%27rps%27%3A+rps%7B%27file_url%27%3Aasset-%3Eurl%7D%2C%27sse%27%3A+sse%7B%27file_url%27%3Aasset-%3Eurl%7D%7D'
+const sanityApi = 'https://ilbimbym.apicdn.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%27temp%27%5D%7Bname%2C%27rps%27%3A+rps%7B%27file_url%27%3Aasset-%3Eurl%7D%2C%27sse%27%3A+sse%7B%27file_url%27%3Aasset-%3Eurl%7D%7D'
 
 // 最后更新时间字段
 const lastUpdateDate = ref('')
