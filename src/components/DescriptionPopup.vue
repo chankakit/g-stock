ed9a34b40b63826e6c5d37168d48475f476713a4
jerlin <91647085+jerlinn@users.noreply.github.com>
2024-05-02 17:04:50 +0800

    Update DescriptionPopup.vue
    「说明」优化

--- src/components/DescriptionPopup.vue
+++ src/components/DescriptionPopup.vue
@@ -24,16 +24,18 @@
             <div class="pop-up-title">全 A 金股雷达</div>
             <p class="pop-up-text">每个交易日收盘后，自动选出<span style="text-decoration: underline;">强度达标且量价齐升的</span>「每日优质股池」，适用于趋势交易。</p>
             <p class="pop-up-text" >RPS ：计算全 A 股的相对强度排名，例：RPS 60 = 87， 代表 60 天内，某个个股的涨幅超过全市 87% 的股票，表现强势。</p>
+            <br>
             <p class="pop-up-text"><b>信号：</b></p>
             <ul class="pop-up-text">
                 <li>🎯 金股：强势、高性价比。</li>
                 <li>⭐️ 优选：强势、有一定的性价比。</li>
                 <li>谨 慎：领涨的龙头，谨慎追高。</li>
-            </ul>          
+            </ul>
+            <br>
             <p class="pop-up-text"><b>使用：</b></p>
             <ul class="pop-up-text">
-                <li>✦ 达标的「强度均值」为 87，黄色高亮显示。</li>
-                <li>✦ 至少 1 个大周期 RPS 达标，才有追踪价值。</li>
+                <li>✦ 达标——强度均值超过 87，黄色高亮显示。</li>
+                <li>✦ 至少 1 个大周期强度达标，才有追踪价值。（对应 RPS 60、RPS 120、RPS 250）</li>
                 <li>✦ 请结合大盘趋势和其他辅助方法做综合决策。</li>
             </ul>
         </template>
